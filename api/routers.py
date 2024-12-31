daee41c44cfe73691f3b87dfdd24892a70c6c2be
kojamad28 <kojamad28@gmail.com>
2024-12-31 18:47:57 +0900

    updated basic codes

--- api/routers.py
+++ api/routers.py
@@ -10,25 +10,25 @@ from .db import get_session
 router = APIRouter()
 
 @router.get('/users/', response_model=List[models.RetrievedUser])
-def get_users(session: Session = Depends(get_session), offset: int = Query(default=0), limit: int = Query(default=100, lte=100)):
+async def get_users(session: Session = Depends(get_session), offset: int = Query(default=0), limit: int = Query(default=100, lte=100)):
     return crud.retrieve_models(session, models.User, offset, limit)
 
 
 @router.get('/user/{target}', response_model=models.RetrievedUser)
-def get_user(target: str, session: Session = Depends(get_session)):
+async def get_user(target: str, session: Session = Depends(get_session)):
     return crud.retrieve_model(session, models.User, 'name', target)
 
 
 @router.post('/users/', response_model=models.RetrievedUser)
-def create_user(created_user: models.CreatedUser, session: Session = Depends(get_session)):
+async def create_user(created_user: models.CreatedUser, session: Session = Depends(get_session)):
     return crud.create_model(session, models.User, created_user)
 
 
 @router.put('/user/{target}', response_model=models.RetrievedUser)
-def update_user(target: str, updated_user: models.UpdatedUser, session: Session = Depends(get_session)):
+async def update_user(target: str, updated_user: models.UpdatedUser, session: Session = Depends(get_session)):
     return crud.update_model(session, models.User, 'name', target, updated_user)
 
 
 @router.delete('/user/{target}')
-def delete_user(target: str, session: Session = Depends(get_session)):
+async def delete_user(target: str, session: Session = Depends(get_session)):
     return crud.delete_model(session, models.User, 'name', target)
