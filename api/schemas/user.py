09007e9e7bef9bf5629ff32032df71e135cbf699
kojamad28 <kojamad28@gmail.com>
2025-03-15 09:01:17 +0900

    updated schemas

--- api/schemas/user.py
+++ api/schemas/user.py
@@ -1,14 +1,34 @@
-from typing import Optional
+from enum import Enum
+from typing import List, Optional
 
 from sqlmodel import SQLModel, Field
+
+from .common import RetrieveModelsQuery, RetrieveModelQuery
+from api.models.accounts import UserGroup
+
+
+class UserColumn(str, Enum):
+    id: str = 'id'
+    name: str = 'name'
+    email: str = 'email'
+    group: str = 'group'
+
+
+class RetrieveUsersQuery(RetrieveModelsQuery):
+    columns: Optional[List[UserColumn]] = Field(default=None)
+
+
+class RetrieveUserQuery(RetrieveModelQuery):
+    columns: Optional[List[UserColumn]] = Field(default=None)
 
 
 class BaseUser(SQLModel):
     name: str = Field(index=True)
     email: str = Field()
+    group: Optional[UserGroup] = Field(default=None)
 
 
-class CreatedUser(BaseUser):
+class CreatedUserBody(BaseUser):
     pass
 
 
@@ -16,6 +36,6 @@ class RetrievedUser(BaseUser):
     id: int
 
 
-class UpdatedUser(BaseUser):
+class UpdatedUserBody(BaseUser):
     name: Optional[str] = Field(default=None, index=True)
     email: Optional[str] = Field(default=None)
